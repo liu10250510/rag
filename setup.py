52a85664705f93bbad6c7b1887b447b7e967f0ee
Lucy <lucy@Lucys-MacBook-Air.local>
2025-07-17 23:21:15 -0400

    change to use strealit

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
 import getpass
 import os
+import streamlit as st
 def setup_environment():
     try:
-        # load environment variables from .env file (requires `python-dotenv`)
-        from dotenv import load_dotenv
-
-        load_dotenv()
+        OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+        st.write(f"OPENAI_API_KEY: {OPENAI_API_KEY}")
     except ImportError:
         pass
-    #print("LANGSMITH_API_KEY:", os.environ.get("LANGSMITH_API_KEY"))
-
-    os.environ["OPNAI_API_KEY"] = "true"
-
-    if "OPNAI_API_KEY" not in os.environ:
-        os.environ["OPNAI_API_KEY"] = getpass.getpass(
-            prompt="Enter your OPNAI API key (optional): "
-        )
-    if "LANGSMITH_PROJECT" not in os.environ:
-        os.environ["LANGSMITH_PROJECT"] = "Simple RAG Project"
-
+    
 if __name__ == "__main__":
     setup_environment()
     
